439d480cf077edbe1be771482193ba5b00632684
b nnn <castilloo.mariel@gmail.com>
2020-04-18 20:48:54 -0300

    metodos

--- src/app/App.java
+++ src/app/App.java
@@ -1,10 +1,32 @@
 package app;
 
 
+import app.arma.Arma;
+
+import java.util.Scanner;
+
 public class App {
+    public static Scanner Teclado = new Scanner(System.in);
+
     public static void main(String[] args) throws Exception {
 
+        System.out.println("BIENVENIDO A THE LOTR");
+
+        //elegir personaje
+        System.out.println("Elija su personaje");
+
+
+        //elegir arma
+        System.out.println("Elegir arma");
+
+        System.out.println("Acha Doble - Espada - Fecha");
+
+        String arma = Teclado.nextLine();
+       // personaje.seleccionarArma(String arma, List<Arma> armas1);
+
+
+    }
+
     
-} 
 
 }
--- src/app/personaje/Personaje.java
+++ src/app/personaje/Personaje.java
@@ -2,20 +2,58 @@ package app.personaje;
 
 import app.arma.Arma;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Personaje {
 
-    String nombre;
-    int salud; // max 100
-    int stamina;// resistencia
-    List<Arma> armas;// las armas disponibles que tiene que usar
+    private String nombre;
+    private int salud; // max 100
+    private int stamina;// resistencia
+    private List<Arma> armas = new ArrayList<Arma>(); // las armas disponibles que tiene que usar
 
-    public Personaje(String nombre, int salud, int stamina) {
+    public Personaje(String nombre, int salud, int stamina, List<Arma> armas) {
         this.nombre = nombre;
         this.salud = salud;
         this.stamina = stamina;
+        this.armas = armas;
     }
+
+    public void seleccionarArma(String arma, List<Arma> armas1){
+
+       /*for(int i=0; i < armas.size(); i++){
+            armas[i]
+       }*/
+
+       /* switch() {
+            case 1:
+
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                // code block
+                break;
+            default:
+        }*/
+
+        /*for (Arma a: armas1) {
+            if(a.getNombre().equals(arma)){
+                armas.add(a);
+            }
+        }*/
+    }
+
+    public void atacar(){
+
+    }
+
     // Devuelve true si el personaje esta vivo
     public boolean estaVivo() {
         return false;
