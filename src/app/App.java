04764366250ae2260e80c7227658943774fb498b
melisacruzmartinez <melisa.cruzmartinez@gmail.com>
2020-04-20 20:53:26 -0300

    retoques

--- src/app/App.java
+++ src/app/App.java
@@ -12,14 +12,17 @@ public class App {
         System.out.println("BIENVENIDO A THE LOTR");
 
         //elegir personaje
-        //System.out.println("Elija su personaje");
-        //System.out.println("Gandallf = 1 ,,Troll,Orco,Hobbit,Goblin,Enano,Elfo");
+        
+        System.out.println("Elija su personaje");
+        System.out.println("Gandallf = 1 ,,Troll,Orco,Hobbit,Goblin,Enano,Elfo");
 
         //elegir arma
         System.out.println("Elegir arma");
         System.out.println("Espada Aduril = 1 , Espada Sting = 2 , Hacha doble = 3 , Baculo = 4 , Arco y flecha 5 ");
 
         String arma = Teclado.nextLine();
+
+        //personaje.getArma();
         
       // personaje.seleccionarArma(String arma, List<Arma> armas1);
 
